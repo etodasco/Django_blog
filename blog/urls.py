e137e895ee4b44b860780d4aaaf895380488d215
Eric Todasco <erictodasco@me.com>
2025-05-21 09:27:30 -0400

    updated function posts to class view posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page") #/posts/my-first-post
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page") #/posts/my-first-post
 ]
