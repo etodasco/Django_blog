252743ccd0bb223890005c3fbdde059f00a1faa6
Eric Todasco <erictodasco@me.com>
2025-05-21 09:07:35 -0400

    changed blog views to class based views

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 
 # Create your views here.
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
 
 def posts(request):
     all_posts = Post.objects.all().order_by("-date")
